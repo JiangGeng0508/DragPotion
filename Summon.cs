25d6becbb0197ac02b9ee7db43ae854b0e9da10c
ZinC0508 <18335432463@163.com>
2025-08-11 22:24:03 +0800

    Summon

---
+++ Summon.cs
@@ -0,0 +1,30 @@
+using Godot;
+using System;
+
+public partial class Summon : Node2D
+{
+	public Button button;
+	public LineEdit lineEdit;
+	public override void _Ready()
+	{
+		button = GetNode<Button>("VBoxContainer/Button");
+		lineEdit = GetNode<LineEdit>("VBoxContainer/LineEdit");
+	}
+	public void SummonItem()
+	{
+		var id = lineEdit.Text.ToInt();
+		var node = GD.Load<PackedScene>($"res://Scenes/Herbs/{id}.tscn").Instantiate();
+		AddChild(node);
+	}
+	public void OnLineEditChanged(string text)
+	{
+		if (text.Length > 0)
+		{
+			var id = text.ToInt();
+			if (id > 0 && id < 25)
+			{
+				button.Icon = GD.Load<Texture2D>($"res://Assets/Herb/{id}.png");
+			}
+		}
+	}
+}
