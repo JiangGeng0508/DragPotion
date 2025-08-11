1bec08ab8b112f75f61446251fd7c541257212f3
ZinC0508 <18335432463@163.com>
2025-08-11 21:59:48 +0800

    Drag

---
+++ DraggableItem.cs
@@ -0,0 +1,55 @@
+using Godot;
+using System;
+
+public partial class DraggableItem : RigidBody2D
+{
+	[Export]
+	public int HerbIndex { get; set; } = 0;
+	public Line2D DragLine { get; set; }
+	public bool IsDragging { get; set; } = false;
+	public Vector2 DragStartPosition { get; set; }
+	public override void _Ready()
+	{
+		DragLine = GetNode<Line2D>("DragLine");
+	}
+	public override void _PhysicsProcess(double delta)
+	{
+		if (IsDragging)
+		{
+			Vector2 mousePosition = GetLocalMousePosition();
+			var force = (mousePosition - DragStartPosition).Rotated(Rotation);
+			force = force.Normalized() * MathF.Pow(force.Length(), 0.5f) * 1000f;
+			DragLine.Points = [DragStartPosition, mousePosition];
+			// ApplyForce(force, DragStartPosition);
+			ApplyCentralForce(force);
+			ApplyTorque(mousePosition.Angle());
+		}
+	}
+	public void OnInputEvent(Node viewport, InputEvent @event, int ShapeIdx)
+	{
+		if (@event is InputEventMouseButton mouseButtonEvent && mouseButtonEvent.ButtonIndex == MouseButton.Left)
+		{
+			if (mouseButtonEvent.Pressed)
+			{
+				IsDragging = true;
+				DragLine.Visible = true;
+				DragStartPosition = GetLocalMousePosition();
+				LinearDamp = 10f;
+				AngularDamp = 10f;
+			}
+		}
+	}
+	public override void _Input(InputEvent @event)
+	{
+		if (@event is InputEventMouseButton mouseButtonEvent && mouseButtonEvent.ButtonIndex == MouseButton.Left)
+		{
+			if (!mouseButtonEvent.Pressed)
+			{
+				IsDragging = false;
+				DragLine.Visible = false;
+				LinearDamp = 0f;
+				AngularDamp = 0f;
+			}
+		}
+	}
+}
---
+++ Pot.cs
@@ -0,0 +1,13 @@
+using Godot;
+using System;
+
+public partial class Pot : Node2D
+{
+	public void OnBodyEntered(Node2D node)
+	{
+		if (node is DraggableItem item)
+		{
+			GD.Print($"item {item.HerbIndex} entered pot");
+		}
+	}
+}
